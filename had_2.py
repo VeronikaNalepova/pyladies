50881830bc2e270f2229f00242e423b910b88fc3
VeronikaNalepova <48591995+VeronikaNalepova@users.noreply.github.com>
2019-04-28 21:36:27 +0200

    Add files via upload

--- had_2.py
+++ had_2.py
@@ -5,6 +5,9 @@ import math
 
 key_press = set()
 
+batch = pyglet.graphics.Batch()
+pic_snake = pyglet.image.load('green.png')
+ap_food = pyglet.image.load("apple.png")
 
 class State():
 
@@ -14,8 +17,14 @@ class State():
         self.direction = ( 0, 1)
         self.snake_width = 64
         self.snake_height = 64
+        self.snake_sprites = []
 
-    
+    def snake_draw(self):
+        self.snake_sprites.clear()
+        for x, y in self.snake_position:
+            self.snake_sprites.append(pyglet.sprite.Sprite(pic_snake, x=x*64, y=y*64, batch=batch))
+
+        batch.draw()
 
     def snake_move(self):
         old_x, old_y = self.snake_position[-1]
@@ -25,23 +34,20 @@ class State():
         new_head = new_x, new_y
 
         if new_x < 0:
-            exit("Game over")
+            exit("Game over you are dead")
         if new_y < 0:
-            exit("Game over")
+            exit("Game over you are dead")
         if new_x + self.snake_width > window.width:
-            exit("Game over")
+            exit("Game over you are dead")
         if new_y + self.snake_height > window.height:
-            exit("Game over")
+            exit("Game over you are dead")
 
         self.snake_position.append(new_head)
         del self.snake_position[0]
 
 
+
 state = State()
-
-pic_snake = pyglet.image.load('green.png')
-
-ap_food = pyglet.image.load("apple.png")
 
 
 def on_key_press(symbol, modifier):
@@ -56,29 +62,24 @@ def on_key_press(symbol, modifier):
     if symbol == key.LEFT:
         state.direction = (-1, 0)
 
+
 def draw():
     window.clear()
+    state.snake_draw()
 
-    for x, y in state.snake_position:
-        snake = pyglet.sprite.Sprite(pic_snake, x, y)
-    snake.draw()
 
 
 def move(dt):
     state.snake_move()
 
 
-
-
-pyglet.clock.schedule_interval(move, 1/60)
-
+pyglet.clock.schedule_interval(move, 1/6)
 
 window = pyglet.window.Window(640, 640)
 
 window.push_handlers(
     on_draw = draw,
     on_key_press = on_key_press,
-
 )
 
 
